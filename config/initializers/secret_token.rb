50ed061e1c396004e48e0a06c531bc0b14bffcaa
RemyHannequin <remy.hannequin@gmail.com>
2013-01-14 23:44:49 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareYourFastFood::Application.config.secret_token = '6c2404af6370e7cdf8816de4f8ae1b71c4454616ab0da3d17d72725da56bf9e08845d5b24e021a6f0e8a29abfd55cfa4171ddbcdd843e25c70dcb596636afc07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ShareYourFastFood::Application.config.session_store :cookie_store, key: '_share-your-fast-food_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ShareYourFastFood::Application.config.session_store :active_record_store
